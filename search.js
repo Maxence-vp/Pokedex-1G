ab6900e4b1b68a11abb29b5a53985476b8ebd8af
Maxence Ver Poorten <maxence.verpoorten@gmail.com>
2024-02-14 11:28:28 +0100

    init

---
+++ search.js
@@ -0,0 +1,39 @@
+const inputElement = document.querySelector("#search-input");
+const search_icon = document.querySelector("#search-close-icon");
+const sort_wrapper = document.querySelector(".sort-wrapper");
+
+inputElement.addEventListener("input", () => {
+  handleInputChange(inputElement);
+});
+search_icon.addEventListener("click", handleSearchCloseOnClick);
+console.log(search_icon); 
+sort_wrapper.addEventListener("click", handleSortIconOnClick);
+console.log(sort_wrapper); 
+
+function handleInputChange(inputElement) {
+  const inputValue = inputElement.value;
+
+  if (inputValue !== "") {
+    document
+      .querySelector("#search-close-icon")
+      .classList.add("search-close-icon-visible");
+  } else {
+    document
+      .querySelector("#search-close-icon")
+      .classList.remove("search-close-icon-visible");
+  }
+}
+
+function handleSearchCloseOnClick() {
+  document.querySelector("#search-input").value = "";
+  document
+    .querySelector("#search-close-icon")
+    .classList.remove("search-close-icon-visible");
+}
+
+function handleSortIconOnClick() {
+  document
+    .querySelector(".filter-wrapper")
+    .classList.toggle("filter-wrapper-open");
+  document.querySelector("body").classList.toggle("filter-wrapper-overlay");
+}
